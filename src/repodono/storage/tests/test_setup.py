5d7b8385933ca8a8d8ea4d9a7201b5e5db505d7a
Tommy Yu <tommy.yu@auckland.ac.nz>
2015-10-07 18:14:17 +1300

    No needing unittest2

--- src/repodono/storage/tests/test_setup.py
+++ src/repodono/storage/tests/test_setup.py
@@ -3,7 +3,7 @@
 from repodono.storage.testing import REPODONO_STORAGE_INTEGRATION_TESTING  # noqa
 from plone import api
 
-import unittest2 as unittest
+import unittest
 
 
 class TestSetup(unittest.TestCase):
