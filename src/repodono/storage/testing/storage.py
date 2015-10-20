c58e8d757f98649486caf2ef09d8778e994ffefe
Tommy Yu <tommy.yu@auckland.ac.nz>
2015-10-20 14:29:44 +1300

    DummyStorage loader and test.

--- src/repodono/storage/testing/storage.py
+++ src/repodono/storage/testing/storage.py
@@ -1,7 +1,17 @@
 # -*- coding: utf-8 -*-
+from glob import glob
+from os.path import join
+from os.path import relpath
+from os import walk
+
 from repodono.storage.base import BaseStorageBackend
 from repodono.storage.base import BaseStorage
 from repodono.storage.interfaces import IStorageInfo
+
+
+def readfile(fullpath):
+    with open(fullpath) as fd:
+        return fd.read()
 
 
 class IDummyStorageInfo(IStorageInfo):
@@ -13,6 +23,12 @@ class IDummyStorageInfo(IStorageInfo):
 
 
 class DummyStorageBackend(BaseStorageBackend):
+    """
+    Dummy backend based on data in a dictionary in memory.
+    """
+
+    def __init__(self):
+        self._data = {}
 
     def acquire(self, context):
         return DummyStorage(context)
@@ -20,6 +36,33 @@ class DummyStorageBackend(BaseStorageBackend):
     def install(self, context):
         pass
 
+    def load_dir(self, id_, root):
+        result = [
+            {
+                relpath(join(r, f), p): readfile(join(r, f))
+                for r, _, files in walk(p)
+                for f in files
+            }
+            for p in sorted(glob(join(root, '*')))
+        ]
+        self._data[id_] = result
+
 
 class DummyStorage(BaseStorage):
+    pass
+
+
+class DummyFSStorageBackend(BaseStorageBackend):
+    """
+    Dummy backend that provides direct access to file system contents.
+    """
+
+    def acquire(self, context):
+        return DummyFSStorage(context)
+
+    def install(self, context):
+        pass
+
+
+class DummyFSStorage(BaseStorage):
     pass
--- src/repodono/storage/tests/test_dummy_storage.py
+++ src/repodono/storage/tests/test_dummy_storage.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from os.path import join
+from os.path import dirname
 from zope.component import getUtility
 from zope.interface import alsoProvides
 
@@ -9,9 +11,43 @@ from repodono.storage.interfaces import IStorageBackend
 from repodono.storage.interfaces import IStorageInstaller
 
 from repodono.storage.testing import REPODONO_DUMMY_STORAGE_INTEGRATION_TESTING
+from repodono.storage.testing import storage
 from repodono.storage.testing.storage import DummyStorage
+from repodono.storage.testing.storage import DummyStorageBackend
 
 import unittest
+
+path = lambda p: join(dirname(storage.__file__), 'data', p)
+
+
+class DummyStorageTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.backend = DummyStorageBackend()
+
+    def tearDown(self):
+        pass
+
+    def test_loader(self):
+        self.backend.load_dir('test', path('testrepo'))
+        self.assertEqual(self.backend._data, {'test': [
+            {'file1': 'file1-rev0\nThis is a test file.\n',
+             'file2': 'file2-rev0\nThis is also a test file.\n'},
+            {'file1': 'file1-rev1\nThis test file has changed.\n',
+             'file2': 'file2-rev0\nThis is also a test file.\n',
+             'file3': 'A new test file.\n'},
+            {'dir1/f1': 'File 1 in dir1\n',
+             'dir1/f2': 'File 2 in dir1\n',
+             'file2': 'file2-rev0\nThis is also a test file.\n',
+             'file3': 'This is to note that file1 was removed.\n'},
+            {'dir1/dir2/f1': 'first file in dir1\n',
+             'dir1/dir2/f2': 'second file in dir2\n',
+             'dir1/dir3/dir4/dir5/info': 'This is some quite nested file.\n',
+             'dir1/f1': 'File 1 in dir1\n',
+             'dir1/f2': 'File 2 in dir1\n',
+             'file1': 'file2-rev0\nThis is also a test file.\n',
+             'file3': 'This is to note that file1 was removed.\n'}
+        ]})
 
 
 class DummyStorageIntegrationTestCase(unittest.TestCase):
