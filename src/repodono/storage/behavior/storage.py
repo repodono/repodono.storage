45b130a795c10d20fc71e0462b28d1f8f67ed181
Tommy Yu <tommy.yu@auckland.ac.nz>
2015-11-05 19:54:58 +1300

    Testing interactions from the browser
    - Forcing the storage to be required has some interesting consequences
      if there are no available backends.  I still want the fields to be
      required so that single default choices can be simply selected,
    - But what if there are multiple choices and we want users to select?
      Might be better to just have this as not required and have other pages
      deal with an unspecified backend?

--- src/repodono/storage/behavior/storage.py
+++ src/repodono/storage/behavior/storage.py
@@ -1,14 +1,56 @@
 # -*- coding: utf-8 -*-
+from types import MethodType
+from logging import getLogger
 from zope import schema
+from zope.schema.vocabulary import SimpleVocabulary
 from zope.interface import provider
+from zope.component import ComponentLookupError
+from zope.component import getUtility
 
 from plone.autoform.interfaces import IFormFieldProvider
 from plone.autoform import directives
 from plone.supermodel import model
 from z3c.form.interfaces import IAddForm
+from z3c.form.browser.select import SelectFieldWidget
+from z3c.form.interfaces import DISPLAY_MODE
 
 from repodono.storage import _
 from repodono.storage.interfaces import IStorageFactory
+
+logger = getLogger(__name__)
+
+
+# If this is generalized, this might be doable
+# @zope.component.adapter(zope.schema.interfaces.IDisplayIfEmptyChoice,
+#                         zope.interface.Interface,
+#                         z3c.form.interfaces.IFormLayer)
+# @zope.interface.implementer(IFieldWidget)
+def DisplayIfEmptySelectFieldWidget(field, source, request=None):
+    """Checks vocabulary, if empty, force display mode"""
+    widget = SelectFieldWidget(field, source, request)
+    if field.vocabulary:
+        # has vocabulary
+        return widget
+    # Alternatively, adapt for the z3c version of terms?
+    # zope.component.getMultiAdapter(
+    #     (None, request, None, field, SelectWidget(request)),
+    #     interfaces.ITerms)
+    vocab = getUtility(
+        schema.interfaces.IVocabularyFactory, name=field.vocabularyName)(None)
+    if not len(vocab):
+        # bind a new update to disable display mode
+        old_update = widget.update
+
+        def update(self):
+            old_update()
+            self.mode = DISPLAY_MODE
+            # Provide an alternative vocabulary that supply the text
+            # for the lack of choice.
+            value = [_(u'<no choices are available>')]
+            self.terms = SimpleVocabulary.fromValues(value)
+            self.value = value
+        widget.update = MethodType(update, widget)
+    return widget
 
 
 @provider(IFormFieldProvider)
@@ -33,6 +75,7 @@ class IStorageEnabler(model.Schema):
 
     directives.omitted('backend')
     directives.no_omit(IAddForm, 'backend')
+    directives.widget(backend=DisplayIfEmptySelectFieldWidget)
 
 
 def StorageCreate(obj, event):
@@ -41,4 +84,14 @@ def StorageCreate(obj, event):
     """
 
     factory = IStorageFactory(obj)
-    factory.install_storage()
+    try:
+        factory.get_storage_backend()
+    except ComponentLookupError as e:
+        iface, name = e.args
+        logger.warning(
+            'Could not find storage backend "%s" while creating a "%s", '
+            'skipping backend instantiation.', name, obj.portal_type)
+    else:
+        # the specific backend instance may do further component lookups
+        # and fail, which this doesn't and shouldn't care about.
+        factory.install_storage()
--- src/repodono/storage/tests/test_doctests.py
+++ src/repodono/storage/tests/test_doctests.py
@@ -3,16 +3,25 @@ from plone.testing import layered
 import doctest
 import unittest
 from repodono.storage.testing import REPODONO_DUMMY_STORAGE_FUNCTIONAL_TESTING
+from repodono.storage.testing import REPODONO_STORAGE_FUNCTIONAL_TESTING
 
 
+# Ones only need the base implementation.
 tests = (
     'storage.txt',
 )
 
+# Ones need dummy storage
+dummy_tests = (
+    'dummy_storage.txt',
+)
+
 
 def test_suite():
-    return unittest.TestSuite(
-        [layered(doctest.DocFileSuite(f, optionflags=doctest.ELLIPSIS),
-                 layer=REPODONO_DUMMY_STORAGE_FUNCTIONAL_TESTING)
-            for f in tests]
-    )
+    d = [layered(doctest.DocFileSuite(f, optionflags=doctest.ELLIPSIS),
+         layer=REPODONO_DUMMY_STORAGE_FUNCTIONAL_TESTING)
+         for f in dummy_tests]
+    s = [layered(doctest.DocFileSuite(f, optionflags=doctest.ELLIPSIS),
+         layer=REPODONO_STORAGE_FUNCTIONAL_TESTING)
+         for f in tests]
+    return unittest.TestSuite(d + s)
