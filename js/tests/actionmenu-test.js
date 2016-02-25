75bddba0ee0012ca719077d98293a20e5a69fe91
Tommy Yu <tommy.yu@auckland.ac.nz>
2016-02-25 16:13:52 +1300

    View test.

--- js/tests/actionmenu-test.js
+++ js/tests/actionmenu-test.js
@@ -5,8 +5,11 @@ define([
   'pat-registry',
   'mockup-patterns-structure',
   'mockup-patterns-structure-url/js/models/result',
+  'mockup-patterns-structure-url/js/views/app',
+  'mockup-patterns-structure-url/js/views/actionmenu',
   'repodonostorage-url/js/actionmenu',
-], function($, _, expect, registry, Structure, Result, ActionMenu) {
+], function($, _, expect, registry, Structure, Result, AppView, ActionMenuView,
+            ActionMenu) {
   'use strict';
 
   window.mocha.setup('bdd');
@@ -52,4 +55,57 @@ define([
     });
 
   });
+
+  describe('Action menu view test', function() {
+    beforeEach(function() {
+      this.app = new AppView({
+        'buttons': [],
+        'activeColumns': [],
+        'availableColumns': [],
+        'indexOptionsUrl': '',
+        'setDefaultPageUrl': '',
+        'collectionConstructor':
+          'mockup-patterns-structure-url/js/collections/result',
+      });
+    });
+
+    afterEach(function() {
+    });
+
+    it('Document type', function() {
+      var model = new Result({
+        viewURL: 'http://localhost/test',
+        portal_type: 'document'
+      });
+
+      var view = new ActionMenuView({
+        app: this.app,
+        model: model,
+        menuGenerator: 'repodonostorage-url/js/actionmenu'
+      });
+
+      var el = view.render().el;
+      expect($('.open a', el).text()).to.eql('Open');
+      expect($('.download a', el).text()).to.eql('Download');
+    });
+
+    it('Folder type (not document)', function() {
+      var model = new Result({
+        viewURL: 'http://localhost/test',
+        portal_type: 'folder'
+      });
+
+      var view = new ActionMenuView({
+        app: this.app,
+        model: model,
+        menuGenerator: 'repodonostorage-url/js/actionmenu'
+      });
+
+      var el = view.render().el;
+      expect($('.open a', el).text()).to.eql('Open');
+      expect($('.download a', el).length).to.be(0);
+    });
+
+  });
+
 });
